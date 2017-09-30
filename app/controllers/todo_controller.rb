16fff31c760df0294853adb2cea042c58cca36ee
Sebastian Gil <sebagi1703@gmail.com>
2017-09-30 19:23:37 +0000

    dynamicly show todos

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,16 @@
 class TodoController < ApplicationController
-end
-class TodoController < ApplicationController
   def index
+    @todo_description = "Make the Curriculumn"
+    @todo_pomodoro_estimate = 4
+  end 
+  def show
+    todo_id= params[:id]
+    if(todo_id=='1')
+      @description = 'Make the Curriculumn'
+      @estimate = 4
+    elsif (todo_id== '2')
+      @description= 'Buy workshop supplies'
+      @estimate= 3
+    end
   end
 end
